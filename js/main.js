17ce501417819bef42104284ec8de79546cabfd4
Roberto Franco <robertofranco8090@gmail.com>
2022-07-20 16:52:11 +0200

    updated to v.1

--- js/main.js
+++ js/main.js
@@ -5,39 +5,60 @@ drinkContainer = document.querySelector('.container__drinks');
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 function getDrink(e) {
-  document.querySelector('.cocktail__ingredients').innerHTML = '';
+  // document.querySelector('.cocktail__ingredients').innerHTML = '';
   const choice = e.target.closest('div').dataset.id;
   fetch(url + choice)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       const { drinks } = data;
       const drink = drinks[0];
+
+      document.querySelector('.hidden').classList.remove('hidden');
+
+      document.querySelector('.intro').classList.add('hidden');
+
       document.querySelector('.cocktail__name').innerText = drink.strDrink;
 
       document.querySelector('.cocktail__img').src = drink.strDrinkThumb;
 
       const ingredients = [];
+      const measures = [];
 
       for (let i = 1; i < 10; i++) {
-        let prop = 'strIngredient' + i;
-        if (drink[prop] !== null) {
-          ingredients.push(drink[prop]);
+        let ingr = 'strIngredient' + i;
+        let meas = 'strMeasure' + i;
+        if (drink[ingr] !== null) {
+          ingredients.push(drink[ingr]);
+          measures.push(drink[meas]);
         } else {
           break;
         }
       }
 
+      document.querySelector('.cocktail__ingredients').innerHTML = `
+      <p class="ingredients__title">Cocktail Ingredients</p>
+      <div class="ingredients__container"></div>
+      `;
       ingredients.forEach((ing, i) => {
-        document.querySelector('.cocktail__ingredients').insertAdjacentHTML(
+        document.querySelector('.ingredients__container').insertAdjacentHTML(
           'beforeend',
           `
-          <p>${ing}</p>
+          <div class="ingredients">
+
+          <p class="ingredient">${ing}</p>
+          <p class="measure">${measures[i] ? measures[i] : ''}</p>
+          </div>
+          
           `
         );
       });
 
-      document.querySelector('.cocktail__instructions').innerText =
-        drink.strInstructions;
+      document.querySelector('.cocktail__instructions').innerHTML = `
+      <p class="ingredients__title">Cocktail Instructions</p>
+      <p>
+      ${drink.strInstructions}
+      </p>
+      `;
     })
     .catch((err) => {
       console.log(`error ${err}`);
@@ -51,6 +72,7 @@ function getFetch() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       const { drinks } = data;
+      drinkContainer.innerHTML = '';
       drinks.forEach((drink) => {
         drinkContainer.insertAdjacentHTML(
           'beforeend',
